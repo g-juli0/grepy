23acc0f4016fb5be804f75003b81a110d292540b
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2023-12-03 14:21:37 -0500

    project outline

--- src/GrepyDriver.java
+++ src/GrepyDriver.java
@@ -1,5 +1,9 @@
-public class App {
+public class GrepyDriver {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    public static void read_file() {
+        
     }
 }
