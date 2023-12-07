99414a2ad994b180d5ed573714512d57d52c769c
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2023-12-06 23:13:59 -0500

    FiveTuple class complete

--- src/GrepyDriver.java
+++ src/GrepyDriver.java
@@ -3,10 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class GrepyDriver {
+
+    // C:\Users\giann\Desktop\Java\grepy\bin>
+    // java --enable-preview GrepyDriver
+
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 	    System.out.print("Enter name of file: ");
-        read_file(input.next());
+        input.next();
+        //read_file(input.next());
     }
 
     public static void read_file(String fileName) throws FileNotFoundException {
